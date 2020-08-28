3552309b0eb02d48177babf5b9ceffcf7fbc257d
Martin Helmich <m.helmich@mittwald.de>
2020-08-28 09:49:11 +0200

    Increase socket timeout for TER publish

--- Build/publish-version.sh
+++ Build/publish-version.sh
@@ -25,4 +25,4 @@ unzip ../web2pdf_*.zip -d extension
 composer -n init 
 composer -n require --dev helhum/ter-client 'dev-master#2afdb1a04c0975a31ab4450daed732bc5f84ea7f'
 
-vendor/bin/ter-client upload -u "${TER_USERNAME}" -p "${TER_PASSWORD}" -m "Release version ${VERSION}" web2pdf ./extension
+php -d default_socket_timeout=3600 vendor/bin/ter-client upload -u "${TER_USERNAME}" -p "${TER_PASSWORD}" -m "Release version ${VERSION}" web2pdf ./extension
