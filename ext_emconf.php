4d2f9d71de6bbea8afaca9248880df3dfab5c2a2
Philipp Stranghöner <p.stranghoener@mittwald.de>
2026-05-27 10:12:34 +0200

    [TASK] Fix TYPO3 version constraint to 13.4.0-14.3.99 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -37,7 +37,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '6.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-14.99.99',
+            'typo3' => '13.4.0-14.3.99',
         ],
     ],
 ];
